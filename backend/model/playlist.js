aca4faa2b59b6429f0385110eaa0538a07148c05
Muzammil <hasanmuzammil2812@gmail.com>
2024-08-27 00:43:09 +0530

    first commit

--- backend/model/playlist.js
+++ backend/model/playlist.js
@@ -2,44 +2,12 @@
 const mongoose = require('mongoose');
 const { Schema, ObjectId } = mongoose;
 
-// [
-//   {
-//     "uniqueId": 1,
-//     "name": "Playlist1",
-//     "songs": [
-//       {
-//         "title": "Purple - YellowFlower",
-//         "artist": "A.R Rahaman",
-//         "duration": "00:00",
-//         "image": "https://images.pexels.com/photos/670741/pexels-photo-670741.jpeg?auto=compress&cs=tinysrgb&w=640&h=380&dpr=1"
-//       },
-//       {
-//         "title": "Purple - YellowFlower",
-//         "artist": "A.R Rahaman",
-//         "duration": "00:00",
-//         "image": "https://images.pexels.com/photos/670741/pexels-photo-670741.jpeg?auto=compress&cs=tinysrgb&w=640&h=380&dpr=1"
-//       },
-//       {
-//         "title": "Purple - YellowFlower",
-//         "artist": "A.R Rahaman",
-//         "duration": "00:00",
-//         "image": "https://images.pexels.com/photos/670741/pexels-photo-670741.jpeg?auto=compress&cs=tinysrgb&w=640&h=380&dpr=1"
-//       },
-//       {
-//         "title": "Purple - YellowFlower",
-//         "artist": "A.R Rahaman",
-//         "duration": "00:00",
-//         "image": "https://images.pexels.com/photos/670741/pexels-photo-670741.jpeg?auto=compress&cs=tinysrgb&w=640&h=380&dpr=1"
-//       }
-//     ]
-//   }
-// ]
 
 const songSchema = new Schema({
   image: String,
   title: String,
-  artist: String, // Make sure this matches your input data
-  duration: String, // Change to String if needed
+  artist: String, 
+  duration: String, 
   path: String
 });
 
